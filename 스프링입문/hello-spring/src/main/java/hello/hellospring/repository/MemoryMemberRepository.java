2c502d53e96ce8d8bbebca1a8ffcad66f91090cc
SeokHo Kim <kim76744@naver.com>
2021-07-18 15:10:53 +0900

    add : Add MemoryMemberRepositoryTest

--- 스프링입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ 스프링입문/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -32,4 +32,8 @@ public class MemoryMemberRepository implements MemberRepository {
     public List<Member> finalAll() {
         return new ArrayList<>(store.values());
     }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
