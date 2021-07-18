fb1b3ae858c0250393fc74f8b653450939043d71
SeokHo Kim <kim76744@naver.com>
2021-07-18 14:42:51 +0900

    add : Add MemberRepository

--- 스프링입문/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ 스프링입문/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -2,11 +2,11 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.lang.reflect.Member;
+
 import java.util.List;
 import java.util.Optional;
 
-public class MemberRepository {
+public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
