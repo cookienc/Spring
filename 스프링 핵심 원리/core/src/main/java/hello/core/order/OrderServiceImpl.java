4ef7bc18557de4df3ec2e750c7c009a050dc7552
SeokHo Kim <kim76744@naver.com>
2021-07-22 23:00:05 +0900

    style[OrderServiceImpl] : 코드 순서 바꿈

--- 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,12 +11,12 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
 
+    private final DiscountPolicy discountPolicy;
+
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-
-    private final DiscountPolicy discountPolicy;
 
     @Override
     public Order creatOrder(Long memberId, String itemName, int itemPrice) {
