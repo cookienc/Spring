7f2bc0a67200543f6219b24ee0fc89441e673a19
SeokHo Kim <kim76744@naver.com>
2021-07-27 22:57:54 +0900

    fix : 빈 조회 2개 문제 해결
    @Qualifier 사용

--- 스프링 핵심 원리/core/src/test/java/hello/core/xml/XmlAppContext.java
+++ 스프링 핵심 원리/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -12,7 +12,7 @@ public class XmlAppContext {
 
     @Test
     void xmlAppContext() {
-        ApplicationContext ac = new GenericXmlApplicationContext("appconfig.xml");
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
