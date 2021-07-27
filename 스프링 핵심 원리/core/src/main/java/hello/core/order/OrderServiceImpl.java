0e75cbf9269d51f235c643d5cbc44b4329bc0859
SeokHo Kim <kim76744@naver.com>
2021-07-27 16:13:24 +0900

    add : 생성자로 의존관계 주입

--- 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,9 +12,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
-    private final DiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
