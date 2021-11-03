138d68fe58b8451959f6ecb31d2c2eb5009144a8
seokho <kim76744@naver.com>
2021-11-03 17:44:07 +0900

    Testing in Mac

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,7 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-
+// 맥 환경에서 테스트
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
