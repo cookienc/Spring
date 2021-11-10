e3a8f147e076179993e5d2b91038ea331b8735f5
kookiedncream <kim76744@naver.com>
2021-11-10 09:25:01 +0900

    style : 세미콜론 추가“

--- SpringMVC/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ SpringMVC/springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -18,12 +18,12 @@ public class ResponseViewController {
 
     @RequestMapping("/response-view-v2")
     public String responseViewV2(Model model) {
-        model.addAttribute("data", "hello!")
+        model.addAttribute("data", "hello!");
         return "response/hello";
     }
 
     @RequestMapping("/response/hello")
     public void responseViewV3(Model model) {
-        model.addAttribute("data", "hello!")
+        model.addAttribute("data", "hello!");
     }
 }
