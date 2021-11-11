def100e2bcb2660ac9d7565ce141c5586eb4710d
kookiedncream <kim76744@naver.com>
2021-11-11 21:02:23 +0900

    add : 저장완료 메시지 출력

--- SpringMVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ SpringMVC/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -85,7 +86,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item) {
 
 //        Item -> item
@@ -93,7 +94,17 @@ public class BasicItemController {
 
 //        model.addAttribute("item", item); //자동 추가, 생략 가능
 
-        return "redirect:/basic/item" + item.getId();
+        return "redirect:/basic/items" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
