53785b844c5f3854698230d9f7d26d5bf3207845
kookiedncream <kim76744@naver.com>
2021-11-14 16:00:52 +0900

    add : 요구사항 추가

---
+++ SpringMVC2/form-start/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum ItemType {
+
+    BOOK("도서"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+}
+
