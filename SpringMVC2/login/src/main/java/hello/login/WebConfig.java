387e34194d49ab0c567ef055ef10713601a9167f
kookiedncream <kim76744@naver.com>
2021-11-22 15:10:00 +0900

    add : 필터 추가

---
+++ SpringMVC2/login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import lombok.extern.java.Log;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*"); //모든 URL에 적용
+
+        return filterRegistrationBean;
+    }
+}
